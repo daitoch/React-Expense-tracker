0eb21123a4bcb04d766b2e43fe1c2235cf12422e
daitoch <farhanch101@outlook.com>
2020-12-10 23:14:37 +0500

    Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Balance from "./component/Balance"
+import Income from "./component/Income"
+import Transaction from "./component/Transaction"
+import AddTransition from "./component/AddTransaction"
 
 const App = () => {
   return (
     <div>
       <h2> Expense tracker</h2>
+      <div className="container">
+        <Balance />
+        <Income />
+        <Transaction />
+        <AddTransition />
+      </div>
     </div>
   );
 };
